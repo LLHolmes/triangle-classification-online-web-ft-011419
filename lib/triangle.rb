c2d9829588e4efaa7e358c25f3cee4e8c70aaca5
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-30 18:41:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  def initialize(a, side2, side3)
+    @a = a
+    @b = b
+    @side3 = side3
+  end
+  
+  def kind
+    
+  end
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
